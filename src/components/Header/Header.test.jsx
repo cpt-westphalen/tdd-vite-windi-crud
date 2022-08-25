25504ce3c00ff9ba5255055097fd95df89f155e9
cpt-westphalen <rbwestphalen@gmail.com>
2022-08-25 20:27:53 -0300

    New: shopping cart icon in header nav + css adjustments for mobile

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -8,7 +8,19 @@ describe('<Header/>', () => {
 	});
 	it('should have an visible image or icon', () => {
 		render(<Header />);
-		const logo = screen.getByRole('img', { name: /logotype/i });
+		const logo = screen.getByRole('icon', { name: /logotype/i });
 		expect(logo).toBeVisible();
+	});
+	it('should have nav links', () => {
+		render(<Header />);
+		const nav = screen.getByRole('navigation');
+		expect(nav).toBeVisible();
+		const links = screen.getAllByRole('link');
+		expect(links.length).toBeGreaterThan(1);
+	});
+	it('should have a cart icon with link', () => {
+		render(<Header />);
+		const cartIcon = screen.getByRole('link', { name: 'shopping cart' });
+		expect(cartIcon).toBeVisible();
 	});
 });
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,25 @@
 import Link from '../Link';
 import { GiFruitTree } from 'react-icons/gi';
+import { MdShoppingBasket } from 'react-icons/md';
 
 export default function Header(props) {
 	return (
-		<header className="flex h-max sm:h-20 flex-wrap sm:flex-nowrap min-w-full justify-around py-2 border-b-1 border-b-dark-300">
+		<header className="flex h-max sm:h-20 flex-wrap sm:flex-nowrap w-full justify-around md:justify-evenly lg:px-30 py-2 border-b-1 border-b-dark-300">
 			<div className="min-h-10 flex items-center">
 				<GiFruitTree
-					role="img"
+					role="icon"
 					aria-label="Logotype"
 					alt="logotype"
 					className="h-10 text-center flex-shrink-0 w-30"
 				/>
 			</div>
-			<nav className="flex min-w-xs w-full flex-shrink-0 sm:flex-1 items-center justify-center gap-x-5 sm:gap-x-20 px-3 pb-1 pt-2 sm:px-0">
+			<nav className="flex min-w-xs w-full flex-wrap items-center justify-center gap-x-2 gap-y-2 sm:gap-x-20 px-3 pb-1 pt-2 sm:px-0">
 				<Link href="/">Home</Link>
 				<Link href="/products">Products</Link>
 				<Link href="/about">About</Link>
+				<a href="/cart" aria-label="shopping cart">
+					<MdShoppingBasket size="30px" />
+				</a>
 			</nav>
 		</header>
 	);
