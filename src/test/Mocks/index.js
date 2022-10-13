6b0d4a8c3f84e2aaed4553e439ca79d5caf1c20e
rbwestphalen <rbwestphalen@gmail.com>
2022-10-13 20:45:45 -0300

    New: more mocks!

--- src/test/Mocks/index.js
+++ src/test/Mocks/index.js
@@ -1,10 +1,18 @@
+import abobrinha from "../../assets/abobrinha.jpeg";
+import alfaceCrespa from "../../assets/alface-crespa.jpeg";
+import alfaceLisa from "../../assets/alface-lisa.jpeg";
+import bergamota from "../../assets/bergamota.jpeg";
+import brocolis from "../../assets/brocolis.jpeg";
+import couveManteiga from "../../assets/couve-manteiga.jpeg";
+import rucula from "../../assets/rucula.jpeg";
+
 const product = {
 	id: 1001,
 	producer: "Fulano d'Tal",
-	img: 'https://acontecebotucatu.com.br/portal/wp-content/uploads/2020/05/alface.jpeg',
-	title: 'Alface',
-	unit: '220 g',
-	badges: ['organic', 'agroforestry', 'vegan'],
+	img: alfaceLisa,
+	title: "Alface Lisa",
+	unit: "220 g",
+	badges: ["organic", "agroforestry", "vegan"],
 	prices: [2.59, 4.49],
 	stock: 4,
 };
@@ -14,10 +22,60 @@ const productList = [
 	{
 		id: 1002,
 		producer: "Cliclano d'Tal",
-		img: '#',
-		title: 'Rúcula',
-		unit: '150 g',
-		badges: ['organic', 'agroforestry', 'vegan'],
+		img: rucula,
+		title: "Rúcula",
+		unit: "150 g",
+		badges: ["organic", "agroforestry", "vegan"],
+		prices: [3.99, 4.99],
+		stock: 8,
+	},
+	{
+		id: 1003,
+		producer: "Cliclano d'Tal",
+		img: brocolis,
+		title: "Brócolis Híbrido",
+		unit: "250 g",
+		badges: ["organic", "agroforestry", "vegan"],
+		prices: [3.99, 4.99],
+		stock: 8,
+	},
+	{
+		id: 1004,
+		producer: "Cliclano d'Tal",
+		img: abobrinha,
+		title: "Abobrinha Italiana",
+		unit: "150 g",
+		badges: ["vegan"],
+		prices: [3.99, 4.99],
+		stock: 8,
+	},
+	{
+		id: 1005,
+		producer: "Cliclano d'Tal",
+		img: alfaceCrespa,
+		title: "Alface Crespa",
+		unit: "80 g",
+		badges: ["organic", "vegan"],
+		prices: [3.99, 4.99],
+		stock: 8,
+	},
+	{
+		id: 1006,
+		producer: "Cliclano d'Tal",
+		img: bergamota,
+		title: "Bergamota Montenegrina",
+		unit: "300 g",
+		badges: ["organic", "vegan"],
+		prices: [3.99, 4.99],
+		stock: 8,
+	},
+	{
+		id: 1007,
+		producer: "Cliclano d'Tal",
+		img: couveManteiga,
+		title: "Couve Manteiga",
+		unit: "150 g",
+		badges: ["vegan"],
 		prices: [3.99, 4.99],
 		stock: 8,
 	},
