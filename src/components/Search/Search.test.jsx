523244570ab25917003d02222a677f647ed3a538
rbwestphalen <rbwestphalen@gmail.com>
2022-10-14 18:52:58 -0300

    ✅ New Search component tests

--- src/components/Search/Search.test.jsx
+++ src/components/Search/Search.test.jsx
@@ -1,21 +1,33 @@
-import { vi } from 'vitest';
-import { Search } from '.';
-import { render, screen, userEvent } from '../../test/utils/test-utils';
+import { vi } from "vitest";
+import { Search } from ".";
+import { render, screen, userEvent } from "../../test/utils/test-utils";
 
 const fn = vi.fn();
 
-describe('<Search/>', () => {
+describe("<Search/>", () => {
 	afterEach(() => {
 		vi.clearAllMocks();
 	});
-	it('should have a searchbox role', () => {
-		render(<Search />);
-		expect(screen.getByRole('searchbox')).toBeVisible();
-	});
-	it('should call spy function with typed value', async () => {
+	test("any placeholder text", () => {
 		render(<Search handleSearch={fn} />);
-		await userEvent.type(screen.getByRole('searchbox'), 'Organic Apples');
-		await userEvent.click(screen.getByRole('button'));
-		expect(fn).toHaveBeenCalled();
+		expect(screen.getByPlaceholderText(/\w+/i)).toBeVisible();
+	});
+	test("'searchbox' aria-role", () => {
+		render(<Search />);
+		expect(screen.getByRole("searchbox")).toBeVisible();
+	});
+	test("call spy function with typed value on button click", async () => {
+		render(<Search handleSearch={fn} />);
+		const searchString = "Alface";
+		await userEvent.type(screen.getByRole("searchbox"), searchString);
+		await userEvent.click(screen.getByRole("button"));
+		expect(fn).toHaveBeenCalledWith(searchString);
+	});
+	test("call fn on Enter key click", async () => {
+		render(<Search handleSearch={fn} />);
+		const searchString = "Alface";
+		await userEvent.type(screen.getByRole("searchbox"), searchString);
+		await userEvent.keyboard("{Enter}");
+		expect(fn).toHaveBeenCalledWith(searchString);
 	});
 });
